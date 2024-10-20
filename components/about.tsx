38e7c0e9b67a942d0fdb8871e394061d86383769
Abdullah-Alholiel <A.alholaiel@gmail.com>
2024-10-20 21:24:17 +0100

    updates

--- components/about.tsx
+++ components/about.tsx
@@ -1,8 +1,8 @@
 // About.tsx
 
-import React from 'react';
-import { motion } from 'framer-motion';
-import { useTheme } from '@/context/theme-context'; // Import useTheme to access the current theme
+import React from "react";
+import { motion } from "framer-motion";
+import { useTheme } from "@/context/theme-context"; // Import useTheme to access the current theme
 
 const AboutMe = () => {
   const { theme } = useTheme(); // Use the theme context
@@ -11,11 +11,11 @@ const AboutMe = () => {
     animate: {
       opacity: 1,
       y: 0,
-      transition: { delay: 0, duration: 0 } // Instant animation
-    }
+      transition: { delay: 0, duration: 0 }, // Instant animation
+    },
   };
 
-  const highlightStyle = `text-indigo-600 ${theme === 'dark' ? 'dark:text-indigo-400' : ''} font-semibold`;
+  const highlightStyle = `text-indigo-600 ${theme === "dark" ? "dark:text-indigo-400" : ""} font-semibold`;
 
   return (
     <motion.section
@@ -24,12 +24,14 @@ const AboutMe = () => {
       initial="initial"
       animate="animate"
     >
-      <div className={`max-w-4xl mx-auto rounded-xl p-10 ${theme === 'dark' ? 'bg-gray-800' : 'bg-white'} shadow-lg text-center ${theme === 'dark' ? 'text-white' : 'text-black'}`}>
+      <div
+        className={`max-w-4xl mx-auto rounded-xl p-10 ${theme === "dark" ? "bg-gray-800" : "bg-white"} shadow-lg text-center ${theme === "dark" ? "text-white" : "text-black"}`}
+      >
         <motion.h1
           className="text-4xl font-bold mb-6"
           initial={{ scale: 0 }}
           animate={{ scale: 1 }}
-          transition={{ type: 'spring', stiffness: 260, damping: 20, delay: 0 }} // Instant animation
+          transition={{ type: "spring", stiffness: 260, damping: 20, delay: 0 }} // Instant animation
         >
           About Me
         </motion.h1>
@@ -39,7 +41,18 @@ const AboutMe = () => {
           animate={{ x: 0, opacity: 1 }}
           transition={{ delay: 0, duration: 0 }}
         >
-          I'm <span className={highlightStyle}>Abdullah Alholaiel</span>, an <span className={highlightStyle}>innovative Digital Strategist, Consultant and System Developer</span> with a proven track record in leading digital transformation projects, enhancing operational efficiency, and driving revenue growth across diverse sectors. My background spans <span className={highlightStyle}>Aerospace Engineering and a Master's in Applied Computing</span>, providing me with a unique blend of technical expertise and problem-solving abilities.
+          I'm <span className={highlightStyle}>Abdullah Alholaiel</span>, an{" "}
+          <span className={highlightStyle}>
+            innovative Digital Strategist, Consultant and System Developer
+          </span>{" "}
+          with a proven track record in leading digital transformation projects,
+          enhancing operational efficiency, and driving revenue growth across
+          diverse sectors. My background spans{" "}
+          <span className={highlightStyle}>
+            Aerospace Engineering and a Master's in Applied Computing
+          </span>
+          , providing me with a unique blend of technical expertise and
+          problem-solving abilities.
         </motion.p>
         <motion.p
           className="text-lg leading-relaxed mt-4"
@@ -47,7 +60,13 @@ const AboutMe = () => {
           animate={{ x: 0, opacity: 1 }}
           transition={{ delay: 0, duration: 0 }}
         >
-          Leveraging skills in <span className={highlightStyle}>software development, cloud computing, and data analytics</span>, I build and implement solutions that optimize business operations and drive growth. I value collaboration and am eager to tackle complex challenges, learn new skills, and advance technology.
+          Leveraging skills in{" "}
+          <span className={highlightStyle}>
+            software development, cloud computing, and data analytics
+          </span>
+          , I build and implement solutions that optimize business operations
+          and drive growth. I value collaboration and am eager to tackle complex
+          challenges, learn new skills, and advance technology.
         </motion.p>
       </div>
     </motion.section>
